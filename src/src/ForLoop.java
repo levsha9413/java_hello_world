ba4e99703f625e029e0006fe2503391be89e8b1f
Бочкарев Павел <pbochkarev@ussc.ru>
2022-10-23 22:43:51 +0500

    Fot loop

---
+++ src/src/ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    static public void main(String[] args){
+        for(int i=0; i<10; i++ ){  //инициализация и декларация счетчика, условие, операция после итерации
+            System.out.println(i);
+        }
+    }
+}
