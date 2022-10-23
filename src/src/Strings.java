21dd769bd8f7306b2f38cd2a94001433e5be1184
Бочкарев Павел <pbochkarev@ussc.ru>
2022-10-23 22:27:21 +0500

    Strings

---
+++ src/src/Strings.java
@@ -0,0 +1,12 @@
+
+//Ссылочный тип данных String
+public class Strings {
+    public static void main(String[] args){
+        String myString = "Hello"; // String - ссылочный тип данных, является класом, поэтому с большой буквы
+        // Hello  - объект, myString - переменная, которая ссылается на этот объект
+        String name = "Bob";
+        System.out.println(myString + " " + name);
+        System.out.println(name + 25 + 25); //Bob2525
+
+    }
+}
