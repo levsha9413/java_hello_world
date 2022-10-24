23813e911c69c44b29814fa6e6881d16ed29859d
Бочкарев Павел <pbochkarev@ussc.ru>
2022-10-24 20:21:49 +0500

    Do while loops

---
+++ src/src/DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int counter = 0;
+        do{
+
+            System.out.println(counter);
+            counter++;
+        }while(counter<10);//цикл выполнится хотя бы один раз, до того как проверится условие
+    }
+}
